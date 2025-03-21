47b5fe74bf155fc6b4eeb7f04c7d7496ae14e0cc
JacobisEpic <108195485+JacobisEpic@users.noreply.github.com>
2025-03-21 15:18:19 -0400

    node server changed speed

--- network/node-server/DemoServer.js
+++ network/node-server/DemoServer.js
@@ -55,10 +55,10 @@ app.get('/', (req, res) => {
 // Function to map commands to vCommand strings
 function getVCommand(command) {
     switch (command) {
-        case 'w': return "1.000 0.000 0.000";
-        case 'a': return "0.000 0.000 -0.500";
-        case 'd': return "0.000 0.000 0.500";
-        case 's': return "-1.000 0.000 0.000";
+        case 'w': return "0.150 0.000 0.000";
+        case 'a': return "0.000 0.000 -0.150";
+        case 'd': return "0.000 0.000 0.150";
+        case 's': return "-0.150 0.000 0.000";
         case 'x': return "0.000 0.000 0.000";
         default: return null;
     }
